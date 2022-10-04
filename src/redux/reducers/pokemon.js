225e0588615445cf4cf9bf56db93607108143c34
Kumilachew-g <kumilachew1212@gmail.com>
2022-10-04 12:19:05 +0300

    Add reducer

--- src/redux/reducers/pokemon.js
+++ src/redux/reducers/pokemon.js
@@ -1,4 +1,4 @@
-import { ADD_POKEMON } from '../actions/actions';
+import { ADD_POKEMON } from '../../components/actions/actions';
 
 const pokemonReducer = (state = [], action) => {
   switch (action.type) {
