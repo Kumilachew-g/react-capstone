3ace2a00f838e00f514e73abb717cc348dc54f92
Kumilachew-g <kumilachew1212@gmail.com>
2022-10-04 17:29:01 +0300

    Update the components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import Routes from './routes/Routes';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
 
 function App() {
-  return <div className="App">react capstone</div>;
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
