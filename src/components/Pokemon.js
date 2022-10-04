5f8632f18690aac857d1dbb04ecff8c205a92ae8
Kumilachew-g <kumilachew1212@gmail.com>
2022-10-04 20:22:27 +0300

    Fix linter errors

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
