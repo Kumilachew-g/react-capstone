ed25499b06e94ee292ad8163989d1b44191d636b
Kumilachew-g <kumilachew1212@gmail.com>
2022-10-08 13:42:32 +0300

    Remove unnecessary commits

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { Tabs, AppBar } from '@material-ui/core';
 import style from '../assets/styles/nav.module.css';
 import navImage from '../assets/images/pokedex.png';
 
@@ -8,15 +7,13 @@ function Navbar() {
   return (
     <>
       <header className={style.headerNav}>
-        <img src={navImage} className={style.pekedexLogo} alt="Logo" />
+        <img src={navImage} className={style.pekedexLogo} alt='Logo' />
         <ul className={style.mainNav}>
-          <Link to="/" className={style.linkStyle}>
-            Home
-            {' '}
+          <Link to='/' className={style.linkStyle}>
+            Home{' '}
           </Link>
-          <Link to="/about" className={style.linkStyle}>
-            About
-            {' '}
+          <Link to='/about' className={style.linkStyle}>
+            About{' '}
           </Link>
         </ul>
       </header>
