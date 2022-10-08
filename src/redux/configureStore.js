d1dd168030ec45145e8c847d3b980afd335123e7
Kumilachew-g <kumilachew1212@gmail.com>
2022-10-08 13:17:28 +0300

    Update store function

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,8 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers/index';
 
 const initialState = [];
 
-const store = createStore(rootReducer, initialState);
+const store = configureStore(rootReducer, initialState);
 
 export default store;
